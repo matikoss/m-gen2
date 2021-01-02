59aba36b1b0f1abc5804754c4f92cc4dfc8d78ab
matikoss <m.kossakowski0@gmail.com>
2021-01-02 13:10:25 +0100

    New unit types

--- RTSProceduralMapGeneratorV2/Assets/Scripts/WyrmsunMapExporting/WyrmUnitsTypes.cs
+++ RTSProceduralMapGeneratorV2/Assets/Scripts/WyrmsunMapExporting/WyrmUnitsTypes.cs
@@ -4,7 +4,6 @@
     {
         public static readonly string SMALL_GOLD = "unit-gold-rock";
         public static readonly string BIG_GOLD = "unit-gold-deposit";
-        public static readonly string SMALL_COPPER = "unit-copper-rock";
         public static readonly string BIG_COPPER = "unit-copper-deposit";
         public static readonly string DWARF_HALL = "unit-ettin-town-hall";
         public static readonly string GERMAN_HALL = "unit-germanic-town-hall";
@@ -12,5 +11,8 @@
         public static readonly string GOBLIN_WORKER = "unit-goblin-worker";
         public static readonly string GERMAN_WORKER = "unit-germanic-worker";
         public static readonly string DWARF_WORKER = "unit-dwarven-miner";
+        public static readonly string SMALL_STONE = "unit-stone-pile";
+        public static readonly string SMALL_WOOD = "unit-wood-pile";
+        public static readonly string SMALL_COPPER = "unit-copper-rock";
     }
 }
